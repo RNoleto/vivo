e7f70f96c793a9312b60f4da76b9f1ac1e7d0c94
Ramon Noleto <devnoleto@gmail.com>
2025-04-07 15:03:20 -0300

    Ajuste de link das avaliações do App

--- src/views/Avaliacoes.vue
+++ src/views/Avaliacoes.vue
@@ -7,10 +7,10 @@ import Card from '../components/ui/Card.vue';
         <Card text="Nos avalie no Google" icon="/icons/maps.svg" link="https://g.co/kgs/C9xaEaS" />
 
         <Card text="App Vivo para iPhone" icon="/icons/apple.svg"
-            link="https://apps.apple.com/br/app/meu-vivo/id123456" />
+            link="https://apps.apple.com/br/app/vivo/id475915064" />
 
         <Card text="App Vivo para Android" icon="/icons/android.svg"
-            link="https://play.google.com/store/apps/details?id=com.vivo.meuvivo" />
+            link="https://play.google.com/store/apps/details?id=br.com.vivo" />
 
         <!-- <Card text="Nossos vendedores" icon="/icons/seller.svg" link="/vendedores" :internal="true" /> -->
     </section>
